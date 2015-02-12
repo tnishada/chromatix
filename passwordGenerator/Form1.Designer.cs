3e5d3bd1a301659cf0550a10dc8548df1e3cbfe9
Tharindu Nishada <lnishada@gmail.com>
2015-02-12 22:51:37 +0530

    3 boxes , min max word count box , maximum string length w/o spaces limited

--- passwordGenerator/Form1.Designer.cs
+++ passwordGenerator/Form1.Designer.cs
@@ -32,12 +32,23 @@
             this.button1 = new System.Windows.Forms.Button();
             this.gen = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textWord1 = new System.Windows.Forms.TextBox();
+            this.textWord2 = new System.Windows.Forms.TextBox();
+            this.textWord3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textMax = new System.Windows.Forms.TextBox();
+            this.textMin = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(3, 50);
+            this.button1.Location = new System.Drawing.Point(3, 102);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(395, 88);
             this.button1.TabIndex = 0;
@@ -46,7 +57,7 @@
             // 
             // gen
             // 
-            this.gen.Location = new System.Drawing.Point(3, 24);
+            this.gen.Location = new System.Drawing.Point(3, 65);
             this.gen.Name = "gen";
             this.gen.Size = new System.Drawing.Size(395, 20);
             this.gen.TabIndex = 1;
@@ -54,17 +65,117 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(301, 149);
+            this.label1.Location = new System.Drawing.Point(301, 226);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = " © Sonic Ad Media";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Word 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Word 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(301, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Word 3";
+            // 
+            // textWord1
+            // 
+            this.textWord1.Location = new System.Drawing.Point(12, 29);
+            this.textWord1.Name = "textWord1";
+            this.textWord1.Size = new System.Drawing.Size(100, 20);
+            this.textWord1.TabIndex = 4;
+            // 
+            // textWord2
+            // 
+            this.textWord2.Location = new System.Drawing.Point(148, 29);
+            this.textWord2.Name = "textWord2";
+            this.textWord2.Size = new System.Drawing.Size(100, 20);
+            this.textWord2.TabIndex = 4;
+            // 
+            // textWord3
+            // 
+            this.textWord3.Location = new System.Drawing.Point(280, 29);
+            this.textWord3.Name = "textWord3";
+            this.textWord3.Size = new System.Drawing.Size(100, 20);
+            this.textWord3.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Text File Word Count";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Max :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(157, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Min :";
+            // 
+            // textMax
+            // 
+            this.textMax.Location = new System.Drawing.Point(51, 223);
+            this.textMax.Name = "textMax";
+            this.textMax.Size = new System.Drawing.Size(100, 20);
+            this.textMax.TabIndex = 6;
+            // 
+            // textMin
+            // 
+            this.textMin.Location = new System.Drawing.Point(193, 223);
+            this.textMin.Name = "textMin";
+            this.textMin.Size = new System.Drawing.Size(100, 20);
+            this.textMin.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 165);
+            this.ClientSize = new System.Drawing.Size(402, 248);
+            this.Controls.Add(this.textMin);
+            this.Controls.Add(this.textMax);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textWord3);
+            this.Controls.Add(this.textWord2);
+            this.Controls.Add(this.textWord1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gen);
             this.Controls.Add(this.button1);
@@ -80,6 +191,17 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox gen;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textWord1;
+        private System.Windows.Forms.TextBox textWord2;
+        private System.Windows.Forms.TextBox textWord3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textMax;
+        private System.Windows.Forms.TextBox textMin;
     }
 }
 
--- passwordGenerator/Form1.cs
+++ passwordGenerator/Form1.cs
@@ -31,11 +31,27 @@ namespace passwordGenerator
        public static Random rd = new Random();
         private void button1_Click(object sender, EventArgs e)
         {
-            if (lines.Length < 100) {
-                MessageBox.Show("Word count is less than 100");
+            int minTextFileWords, maxTextFileWords;
+
+            try
+            {
+                minTextFileWords = Convert.ToInt32(textMin.Text);
+                maxTextFileWords = Convert.ToInt32(textMax.Text);
+
+                if (minTextFileWords > maxTextFileWords || textMax.Text == "" || textMin.Text == "" ) {
+                    throw new System.InvalidOperationException();
+                }
+            }
+            catch(Exception et) {
+                minTextFileWords = 85;
+                maxTextFileWords = 100;               
+            }
+            
+            if (lines.Length < maxTextFileWords) {
+                MessageBox.Show("Word count is less than "+maxTextFileWords);
                 Environment.Exit(0);
             }
-
+            
             int length = 160 + rd.Next(11);
             string randomText = CreateRandomPassword(length);
 
@@ -45,15 +61,44 @@ namespace passwordGenerator
             }
 
             // randomly select the number of words to be inserted
-            int wordsToBeInserted = rd.Next(85, 100);
+            int wordsToBeInserted = rd.Next(minTextFileWords, maxTextFileWords);
 
             for (int index = 0; index < wordsToBeInserted; index++)
             {
                 //select a word randomly from the list
                 randomText += " " + lines[rd.Next(0, lines.Length - 1)];
             }
-                        
-            gen.Text = randomText;
+
+            // adding 3 special words to 5th 8th 12th spaces
+            int whiteSpaces = 0;
+            for (int index = 0; index < randomText.Length; index++)
+            {
+                if (randomText[index] == ' ')
+                {
+                    whiteSpaces++;
+
+                    if (whiteSpaces == 8)
+                    { 
+                        randomText = randomText.Substring(0 , index) + " " + textWord1.Text +" "+ randomText.Substring(index+1) ;
+                    }
+
+                    if (whiteSpaces == 15)
+                    {
+                        randomText = randomText.Substring(0 , index) + " " + textWord2.Text + randomText.Substring(index+1); 
+                   
+                    }
+
+                    if (whiteSpaces == 25)
+                    {
+                        randomText = randomText.Substring(0, index) + " " + textWord3.Text + randomText.Substring(index + 1);
+                   
+                    }
+                }
+            }
+
+            
+
+                gen.Text = randomText;
             System.Windows.Forms.Clipboard.SetText(gen.Text); // copy to the clipboard
 
             /*select all text*/
@@ -67,13 +112,14 @@ namespace passwordGenerator
             string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
             char[] chars = new char[passwordLength];
            
-
+            //generate a random text string
             for (int i = 0; i < passwordLength; i++)
             {
                 chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
                 
             }
 
+            //put spaces beside a and i letters
             for (int index = 0; index < chars.Length; index++)
             {
                 if ((index >1  && index < chars.Length - 3) && ((chars[index]) == 'A' || chars[index] == 'a' || chars[index] == 'i' || chars[index] == 'I'))
@@ -82,6 +128,30 @@ namespace passwordGenerator
                     chars[index + 1] = ' ';
                 }
             }
+
+            //reduce char length w/o white spaces 
+            int counter = 0;
+            for (int index = 0; index < chars.Length; index++) 
+            {
+                if (chars[index] == ' ')
+                {
+                    counter = 0;
+                }
+                else {
+                    counter++;
+                }
+
+                //maximum length of a string componenet without white spaces is 12 chars
+                if (counter > 12) 
+                {
+                    int rdval = rd.Next(2, 11);
+                    index = index - rdval;
+                    chars[index] = ' ';
+                    counter = 0;
+                }
+            }
+
+           
 
                 return new string(chars);
         }
